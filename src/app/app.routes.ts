f8cda3a26153a7d7d1f14efddd27c82ee1ef65aa
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-03-06 23:40:22 +0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { ExtraOptions, Routes } from '@angular/router';
 import { CourserInfoComponent } from './components/courser-info/courser-info.component';
 import { CourserResumeComponent } from './components/courser-resume/courser-resume.component';
 import { ApplyCourseComponent } from './components/apply-course/apply-course.component';
+import { CertificatesComponent } from './components/certificates/certificates.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 export const extraOptions: ExtraOptions = {scrollPositionRestoration:'enabled', anchorScrolling:'enabled' }
 export const routes: Routes = [
     {path:"", pathMatch:"full", redirectTo:"courserInfo"},
     {path:"courserInfo", component:CourserInfoComponent},
     {path:"courserResume", component:CourserResumeComponent},
-    {path:"applyCourse", component:ApplyCourseComponent}
+    {path:"applyCourse", component:ApplyCourseComponent},
+    {path:"certificates", component:CertificatesComponent},
+    {path:"portfolio", component:ProjectsComponent},
   ];
