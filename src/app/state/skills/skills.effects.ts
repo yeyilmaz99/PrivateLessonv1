06fa83fd46b05d3022ab5c6cd13e501ba01f057b
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-05-11 19:51:57 +0300

    update

--- src/app/state/skills/skills.effects.ts
+++ src/app/state/skills/skills.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { SkillService } from '../../services/skillService/skill.service';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { loadSkills, loadSkillsSuccess } from './skills.actions';
-import { map, mergeMap } from 'rxjs';
+import { loadSkills, loadSkillsSuccess, updateSkill, updateSkillSuccess } from './skills.actions';
+import { map, mergeMap, of } from 'rxjs';
 
 @Injectable()
 export class SkillsEffects {
@@ -22,4 +22,17 @@ export class SkillsEffects {
       })
     );
   });
+
+  updateSkill$ = createEffect(() => {
+    return this.actions$.pipe(ofType(updateSkill), mergeMap((action) => {
+        // this.store.dispatch(setLoadingSpinner({status:true, from:"update brand"}))
+      return this.skillService.updateSkill(action.id, action.skill).pipe(mergeMap((data) => {
+        const updateSkillSuccessAction = updateSkillSuccess();
+        // this.store.dispatch(setLoadingSpinner({status:false, from:"update brand success"}))
+        return of(updateSkillSuccessAction, loadSkills());
+      }))
+    }))
+  })
+
+  
 }
