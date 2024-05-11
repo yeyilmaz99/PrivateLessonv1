661855035c15a405d7e9a8a193f6f970410b307f
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-05-11 19:51:54 +0300

    update

--- src/app/state/skills/skills.actions.ts
+++ src/app/state/skills/skills.actions.ts
@@ -1,8 +1,17 @@
 import { createAction, props } from '@ngrx/store';
-import { Skill } from '../../models/skillModel';
+import { Skill, SkillForUpdate } from '../../models/skillModel';
 
 export const LOAD_SKILLS = '[Skills] Load Skills';
 export const LOAD_SKILLS_SUCCESS = '[Skills] Load Skills Success';
+
+
+export const UPDATE_SKILL = '[Skills] Update Skill';
+export const UPDATE_Skill_SUCCES ='[Skill] Update Skill Success';
+
+
+export const updateSkill = createAction(UPDATE_SKILL, props<{id:number,skill:SkillForUpdate}>())
+export const updateSkillSuccess = createAction(UPDATE_Skill_SUCCES);
+
 
 export const loadSkills = createAction(LOAD_SKILLS);
 export const loadSkillsSuccess = createAction(
