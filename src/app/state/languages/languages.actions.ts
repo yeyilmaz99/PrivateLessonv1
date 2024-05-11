351b94b516f3d64762fd37b097dc0ee451e68ee9
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-05-11 19:51:47 +0300

    update

--- src/app/state/languages/languages.actions.ts
+++ src/app/state/languages/languages.actions.ts
@@ -1,8 +1,16 @@
 import { createAction, props } from '@ngrx/store';
-import { Language } from '../../models/languageModel';
+import { Language, LanguageForUpdate } from '../../models/languageModel';
 
 export const LOAD_LANGUAGES = '[Language] Load Languages';
 export const LOAD_LANGUAGES_SUCCESS = '[Language] Load Languages Success';
+
+export const UPDATE_LANGUAGE = '[Language] Update Language';
+export const UPDATE_LANGUAGE_SUCCESS ='[Language] Update Language Success';
+
+
+export const updateLanguage = createAction(UPDATE_LANGUAGE, props<{id:number,language:LanguageForUpdate}>())
+export const updateLanguageSuccess = createAction(UPDATE_LANGUAGE_SUCCESS);
+
 
 export const loadLanguages = createAction(LOAD_LANGUAGES);
 export const loadLanguagesSuccess = createAction(
