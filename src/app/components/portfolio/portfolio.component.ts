6b813dc4289591a6e3b1db4b357332ae84161c41
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-05-12 20:50:51 +0300

    update

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -3,23 +3,28 @@ import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../state/reducers';
 import { getTextByType } from '../../state/texts/texts.selector';
-import { FEndText } from '../../models/textsModel';
-import { loadMainPhotos } from '../../state/photos/photos.actions';
+import { FETextToUpdateDto, FEndText } from '../../models/textsModel';
+import { loadMainPhotos, updatePhoto } from '../../state/photos/photos.actions';
 import { getMainPhotos } from '../../state/photos/photos.selector';
-import { Photo } from '../../models/photoModel';
+import { Photo, PhotoForUpdate } from '../../models/photoModel';
 import { Observable } from 'rxjs';
 import { isAdmin } from '../auth/state/auth.selector';
 import { AsyncPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { updateText } from '../../state/texts/texts.actions';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe, ReactiveFormsModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css',
 })
 export class PortfolioComponent implements OnInit {
   store = inject(Store<AppState>);
+  formBuilder = inject(FormBuilder);
+  toastrService = inject(ToastrService)
   header: FEndText;
   projectHeader1: FEndText;
   projectDescription1: FEndText;
@@ -29,10 +34,14 @@ export class PortfolioComponent implements OnInit {
   topPhoto: Photo;
   bottomPhoto: Photo;
   isAdmin: Observable<boolean>;
+  updateHeaderForm: FormGroup;
+  updatePortfolyoPhotoForm: FormGroup;
+  updatePortfolyoPhotoForm2: FormGroup;
   ngOnInit(): void {
     this.getTexts();
     this.getMainPhotos();
-    this.isAdmin = this.store.select(isAdmin)
+    this.isAdmin = this.store.select(isAdmin);
+    this.createForms();
   }
 
   getMainPhotos() {
@@ -52,17 +61,102 @@ export class PortfolioComponent implements OnInit {
     this.store.select(getTextByType, 21).subscribe((res) => {
       this.header = res;
     });
-    this.store.select(getTextByType, 6).subscribe((res) => {
-      this.projectHeader1 = res;
-    });
-    this.store.select(getTextByType, 19).subscribe((res) => {
-      this.projectDescription1 = res;
-    });
-    this.store.select(getTextByType, 7).subscribe((res) => {
-      this.projectHeader2 = res;
-    });
-    this.store.select(getTextByType, 20).subscribe((res) => {
-      this.projectDescription2 = res;
-    });
+  }
+
+  createUpdateHeaderForm(){
+    this.updateHeaderForm = this.formBuilder.group( {
+      text:["",Validators.required]
+    })
+  }
+
+  createForms(){
+    this.createUpdateHeaderForm();
+    this.createUpdatePortfolyoPhotoForm();
+    this.createUpdatePortfolyoPhotoForm2();
+  }
+
+  updateTextString(id:number){
+    if(!this.updateHeaderForm.valid){
+      this.toastrService.error("An unexpected error occurred, Please try again");
+      return;
+    }
+    let text: FETextToUpdateDto = Object.assign({}, this.updateHeaderForm.value);
+    this.store.dispatch(updateText({id, text}));
+    this.updateHeaderForm.reset();
+    this.toastrService.success("Successfuly Updated");
+  }
+
+
+ createUpdatePortfolyoPhotoForm(){
+  this.updatePortfolyoPhotoForm = this.formBuilder.group({
+    headerText:[""],
+    description:[""],
+    imageData:[File]
+  })
+ }
+
+ createUpdatePortfolyoPhotoForm2(){
+  this.updatePortfolyoPhotoForm2 = this.formBuilder.group({
+    headerText:[""],
+    description:[""],
+    imageData:[File]
+  })
+ }
+
+
+
+
+
+
+
+
+  updatePortfolyoPhoto(id:number, isMain:boolean, isCertificate:boolean):void{
+    if(!this.updatePortfolyoPhotoForm.valid){
+      this.toastrService.error("Alanlar Boş Olamaz")
+    }
+    let photo: PhotoForUpdate = Object.assign({}, this.updatePortfolyoPhotoForm.value);
+
+    const formData = new FormData();
+
+    const blob = new Blob([photo.imageData], {type:'image/jpeg'})
+    formData.append('ImageData', blob, 'image.png');
+    formData.append('HeaderText', photo.headerText);
+    formData.append('Description',photo.description)
+
+    this.store.dispatch(updatePhoto({id,isMain,isCertificate,formData}));
+    this.updatePortfolyoPhotoForm.reset();
+    this.toastrService.success("Successfuly Updated");
+    this.store.dispatch(loadMainPhotos());
+  }
+
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0]
+    this.updatePortfolyoPhotoForm.patchValue({ imageData: file });
+  }
+
+  onFileSelected2(event: any) {
+    const file = event.target.files[0]
+    this.updatePortfolyoPhotoForm2.patchValue({ imageData: file });
+  }
+
+
+  updatePortfolyoPhoto2(id:number,isMain:boolean,isCertificate:boolean){
+    if(!this.updatePortfolyoPhotoForm2.valid){
+      this.toastrService.error("Alanlar Boş Olmamalıdır.");
+    }else{
+      let photo: PhotoForUpdate = Object.assign({}, this.updatePortfolyoPhotoForm2.value);
+
+      const formData = new FormData();
+      console.log(photo.imageData);
+      const blob = new Blob([photo.imageData], {type:'image/jpeg'})
+      formData.append('ImageData', blob, 'image.png');
+      formData.append('HeaderText', photo.headerText);
+      formData.append('Description',photo.description)
+      this.store.dispatch(updatePhoto({id,isMain,isCertificate,formData}));
+      this.updatePortfolyoPhotoForm2.reset();
+      this.toastrService.success("Successfuly Updated");
+      this.store.dispatch(loadMainPhotos());
+    }
   }
 }
