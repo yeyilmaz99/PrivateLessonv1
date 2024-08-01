093fbc6dd1a35290362613fb23b7c12bcf11d56f
yeyilmaz99 <yeyilmaz34@gmail.com>
2024-08-01 16:56:15 +0300

    update

---
+++ src/app/pipes/day-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DayNamePipe } from './day-name.pipe';
+
+describe('DayNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DayNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
